d9356f91e256c472e0d3baaa8c7ad5cb270a762a
React1 <React1@flyweis.technology>
2023-10-04 19:17:09 +0530

    dsad

---
+++ .history/src/components/AboutUs_20231004184231.js
@@ -0,0 +1,100 @@
+/** @format */
+
+import React, { useEffect, useState } from "react";
+import Pictures from "./home/Pictures";
+import { ServiceCardImages } from "../constants/constant";
+import { Header } from "../utils/helpingComponent";
+import { useNavigate } from "react-router-dom";
+
+const AboutUs = () => {
+  const [show, setShow] = useState(false);
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <section className="bg-primary text-white font-light">
+    <div className="Backward_Heading step_Heading">
+        <div>
+          <img src="/Image/1.png" alt="" onClick={() => navigate(-1)} />
+        </div>
+        <p className="title">About Us
+</p>
+      </div>
+      <Header styles={"text-white"} heading={"About Us"} />
+      <div className="flex justify-center  gap-14">
+        <div className="w-[37rem] h-[37rem]  border-8 border-secondary overflow-hidden">
+          <img
+            className="w-full h-full object-cover"
+            alt="aboutus"
+            src={ServiceCardImages[0].src}
+          />
+        </div>
+        <div
+          className="flex flex-col w-[47.04rem] items-start"
+          style={{ gap: "20px" }}
+        >
+          <h3 className="text-4xl font-medium">Who We Are</h3>
+          <h4 className="font-medium text-xl flex items-center gap-2">
+            Shahina Hoja, RN, LE
+            <span className="font-light text-sm ">
+              - Aesthetic Nurse & Founder
+            </span>
+          </h4>
+          <p>
+            A licensed Aesthetic Nurse and founder of Shahina Hoja Aesthetics.
+            Shahina became Board certified Skin therapist in 2017. 5 years
+            later, and she still continues to grow more passionate about
+            increasing self - love and confidence for all her clients!
+          </p>
+          {show && (
+            <>
+              {" "}
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac
+                neque felis. Praesent id fermentum tellus, nec sagittis urna.
+                Integer gravida, libero vel auctor varius, nibh nisl egestas
+                tortor, vitae facilisis sapien sem non sapien. Phasellus vitae
+                ipsum vitae elit pretium tempus. Maecenas id tortor leo. Morbi
+                ornare viverra magna, eget rutrum tortor consequat ac. Donec eu
+                diam ante. Curabitur sed erat ultricies, pellentesque justo a,
+                porta nibh.
+              </p>
+              <p>
+                Sed ultricies mi augue, non ultrices augue semper sit amet.
+                Suspendisse molestie, neque lacinia iaculis finibus, sapien
+                turpis aliquet sem, ut tincidunt nunc justo sed elit. Nam
+                viverra finibus turpis a facilisis. Proin hendrerit scelerisque
+                dignissim. Integer pretium rutrum elit ac pulvinar. In gravida
+                commodo turpis eu aliquam. P
+              </p>
+            </>
+          )}
+
+          {show ? (
+            <button
+              className="w-96 py-6 bg-secondary text-primary text-2xl font-bold"
+              onClick={() => setShow(false)}
+            >
+              SHOW LESS
+            </button>
+          ) : (
+            <button
+              className="w-96 py-6 bg-secondary text-primary text-2xl font-bold"
+              onClick={() => setShow(true)}
+            >
+              VIEW MORE
+            </button>
+          )}
+        </div>
+      </div>
+
+      <hr className="bg-secondary mt-[28.5rem] mb-14 w-3/4 mx-auto" />
+      <Pictures />
+    </section>
+  );
+};
+
+export default AboutUs;
---
+++ .history/src/components/productPage/ProductPage_20231004172722.js
@@ -0,0 +1,86 @@
+/** @format */
+
+import React from "react";
+import { BiSearch } from "react-icons/bi";
+import AllProducts from "./AllProducts";
+import Options from "./Options";
+import { useNavigate } from "react-router-dom";
+
+const skintype = [
+  "All Skin Types",
+  "Aging",
+  "Dry",
+  "Normal",
+  "Oily",
+  "Sensitive",
+];
+const productType = ["Moisturizer", "Cleanser", "Toner"];
+const skinConditions = ["Aging", "Pigmentation", "Rosacea", "Sensitive"];
+
+const ProductPage = () => {
+  const navigate = useNavigate();
+  
+  // React.useEffect(() => {
+  //   window.scrollTo(0, 0);
+  // }, []);
+
+
+  return (
+    <section>
+
+
+      <div className="relative_product_container">
+        <img
+          className="full_Image"
+          src="/Image/39.jpg"
+          alt=""
+        />
+        <div className="absolute bottom-0 w-full h-full flex flex-col bg-black bg-opacity-25  justify-around">
+          <h1 className="text-6xl text-white text-center  z-50 font-light">
+            DMK Products
+          </h1>
+        </div>
+        <div
+          onClick={() => navigate(-1)}
+          className="absolute cursor-pointer top-24 left-12"
+        >
+          <img src="/asessts/back-button.svg" alt="back-button" />
+        </div>
+      </div>
+
+
+
+
+
+
+      <section style={{maxWidth : '1400px' , margin  : 'auto'}}>
+        <div className="relative">
+          <h1 className="text-4xl text-center font-medium text-primary my-20">
+            ALL PRODUCTS
+          </h1>
+          <div className="absolute top-0 right-12 flex gap-5 items-center text-xl border-b-2 pb-2   w-80 border-b-primary text-primary">
+            <BiSearch className="text-3xl" />
+            <input
+              className="px-2"
+              type="text"
+              placeholder="Search Products...."
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-shrink-0 justify-between items-start">
+          <div className=" w-[30%] px-12 py-4">
+            <Options type={"SKIN TYPE"} options={skintype} />
+            <Options type={"PRODUCT TYPE"} options={productType} />
+            <Options type={"SKIN CONDITION"} options={skinConditions} />
+          </div>
+          <div className="w-full border-l-2 border-l-primary mb-20">
+            <AllProducts />
+          </div>
+        </div>
+      </section>
+    </section>
+  );
+};
+
+export default ProductPage;
