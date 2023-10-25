bb3290a34146ef4afb2ac4c9f52eed7121117434
React1 <React1@flyweis.technology>
2023-10-25 12:03:48 +0530

    12:03

---
+++ .history/src/components/AllNews_20231024124511.js
@@ -0,0 +1,53 @@
+/** @format */
+
+import React, { useState, useEffect } from "react";
+import { getNews } from "../Repository/Api";
+
+const AllNews = () => {
+  const [response, setResponse] = useState([]);
+
+  function fetchHandler() {
+    getNews(setResponse);
+  }
+
+  useEffect(() => {
+    fetchHandler();
+  }, []);
+
+  const textTransformation = (content) => {
+    if(content?.length > 200) {
+      return (
+        ``
+      )
+    }
+  }
+
+  return (
+    <section
+      className=" Home_Three_Sec  MaxComponent All_News_Page"
+      style={{ marginBottom: "40px" }}
+    >
+      <div className="gap-3  mx-auto  All_News_Page">
+        {response?.map((card, index) => (
+          <section className="p-3" key={index}>
+            <div className="my-2">
+              <img src={card.image} className="w-full h-auto" alt="news" />
+              <h3 className="my-6 font-medium text-3xl text-primary title">
+                {card.title}
+              </h3>
+              <p className="text-xl font-normal desc">{card.description}</p>
+
+              <div className="flex justify-center my-3 viewMore-Container">
+                <button className="w-1/4 py-2 font-bold text-primary bg-secondary viewMore">
+                  VIEW MORE
+                </button>
+              </div>
+            </div>
+          </section>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default AllNews;
---
+++ .history/src/components/home/NewsCard_20231024123149.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const NewsCard = ({ src, title, content }) => {
+  const navigate = useNavigate()
+
+
+  const textTransformation = (content) => {
+    if(content?.length > 100) {
+      return ( 
+        `${content?.substr(0,100)}...`
+      )
+    }else{
+      return content
+    }
+  }
+
+  return (
+    <section className="p-3 w-[430px] NewsCard ">
+      <div className="my-2">
+        <img src={src} className="w-full h-auto" alt="news" />
+        <h3 className="my-6 font-medium text-3xl text-primary title">{title}</h3>
+        <p className="text-xl font-normal desc">{content} {textTransformation(content)} </p>
+      </div>
+      <div className="flex items-center gap-2 font-semibold text-lg mt-6 cursor-pointer" onClick={() => navigate('/allNews')} >
+        READ MORE <img src="/asessts/arrow.svg" alt="arrow" />
+      </div>
+    </section>
+  );
+};
+
+export default NewsCard;
