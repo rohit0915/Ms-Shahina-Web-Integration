c5c57bb1ed8975e7ab59a3fd4034832d14b408a3
React1 <React1@flyweis.technology>
2024-01-06 19:07:36 +0530

    new collection

---
+++ .history/src/components/Stripe/StripeComp_20240106184010.js
@@ -0,0 +1,94 @@
+/** @format */
+
+import { useState } from "react";
+import {
+  useStripe,
+  useElements,
+  PaymentElement,
+} from "@stripe/react-stripe-js";
+import { useDispatch } from "react-redux";
+import { serviceCheckout } from "../../Repository/Api";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+export default function StripeComp() {
+  const stripe = useStripe();
+  const elements = useElements();
+  const [errorMessage, setErrorMessage] = useState();
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [status, setStatus] = useState(null);
+
+  const handleError = (error) => {
+    setLoading(false);
+    setErrorMessage(error.message);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (!stripe) {
+      return;
+    }
+
+    setLoading(true);
+
+    const { error: submitError } = await elements.submit();
+    if (submitError) {
+      handleError(submitError);
+      return;
+    }
+
+    try {
+      const res = await axios.post(
+        `${process.env.React_App_Baseurl}api/v1/user/card/savecard`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("Token")}`,
+          },
+        }
+      );
+      const clientSecret = res?.data?.client_secret?.client_secret;
+      await dispatch(serviceCheckout(status));
+      const { error } = await stripe.confirmSetup({
+        elements,
+        clientSecret,
+        confirmParams: {
+          return_url: "https://shahina-web.vercel.app/mycart",
+        },
+      });
+      if (error) {
+        handleError(error);
+      } else {
+      }
+    } catch (error) {
+      handleError(error);
+    }
+  };
+
+  const btnStyle = {
+    backgroundColor: "#000",
+    color: "#fff",
+    padding: "10px",
+    borderRadius: "10px",
+    border: "1px solid #000",
+    width: "200px",
+    display: "block",
+    marginTop: "15px",
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <PaymentElement />
+      <div className="checked_check">
+        <input type="checkbox" required />
+        <p>I agree cancellation policy</p>
+      </div>
+      <button style={btnStyle} type="submit" disabled={!stripe || loading}>
+        Book Now
+      </button>
+      {errorMessage && <div>{errorMessage}</div>}
+    </form>
+  );
+}
