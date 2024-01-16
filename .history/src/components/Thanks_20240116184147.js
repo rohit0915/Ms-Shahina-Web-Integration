3bfa3032bf3226c90d30a4cbf634789cd24b2d09
React1 <React1@flyweis.technology>
2024-01-16 19:11:45 +0530

    changes

---
+++ .history/src/components/Thanks_20240116184147.js
@@ -0,0 +1,18 @@
+/** @format */
+
+import React, { useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+
+const Thanks = () => {
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "instant",
+    });
+  }, []);
+
+  return
+ ;
+};
+
+export default Thanks;
---
+++ .history/src/components/shop/ProductCard_20240116173757.js
@@ -0,0 +1,22 @@
+/** @format */
+
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const ProductCard = ({ src, title, price, id }) => {
+  const navigate = useNavigate();
+  return (
+    <div
+      className="Item cursor-pointer  "
+      onClick={() => navigate(`/product/${id}`)}
+    >
+      <div className="thumbnail">
+        <img className="w-full h-full" src={src} alt="" />
+      </div>
+      <p className="text-2xl font-black text-primary">${price}</p>
+      <h4 className="text-xl font-normal">{title}</h4>
+    </div>
+  );
+};
+
+export default ProductCard;
