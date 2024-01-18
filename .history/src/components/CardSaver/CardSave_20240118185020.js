2deaf224d9d6eeec1a79ed3c0104a07bb610770d
React1 <React1@flyweis.technology>
2024-01-18 19:04:18 +0530

    18/01/2024

---
+++ .history/src/components/CardSaver/CardSave_20240118185020.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CardSave = () => {
+  return (
+    <div>CardSave</div>
+  )
+}
+
+export default CardSave
---
+++ .history/src/components/VerifySubScription_20240118162830.js
@@ -0,0 +1,46 @@
+/** @format */
+
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { verifySubscription } from "../Repository/Api";
+
+const VerifySubScription = () => {
+  const { id } = useParams();
+  const [response, setResponse] = useState(false);
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "instant",
+    });
+  }, []);
+
+  useEffect(() => {
+    verifySubscription(id, setResponse);
+  }, [id]);
+
+  return response === false ? (
+    <div className="Thanks_Container">
+      <p className="desc">
+        Our system is diligently checking the details to confirm your
+        subscription. <br /> This process may take a few seconds, but rest
+        assured, we appreciate your patience.
+      </p>
+    </div>
+  ) : (
+    <div className="Thanks_Container">
+      <p className="title">Thank You!</p>
+
+      <p className="desc">
+        Congratulations! Thanks for becoming a member. <br /> We are looking
+        forward to provide you the best Experience
+      </p>
+      <img src={"/Image/12.gif"} alt="" />
+      <Link to="/" style={{ cursor: "pointer" }}>
+        <button>RETURN TO HOMEPAGE</button>
+      </Link>
+    </div>
+  );
+};
+
+export default VerifySubScription;
